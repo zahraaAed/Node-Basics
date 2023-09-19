961ac82547cfe0e9e5bcbbd7479e87c4b64454fa
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-09-19 16:33:39 +0300

    help command

--- tasks.js
+++ tasks.js
@@ -3,7 +3,7 @@
  * Starts the application
  * This is the function that is run when the app starts
  * 
- * It prints a welcome line, and then a line with "----",
+ * It prints a welcome line, and then a line with '----',
  * then nothing.
  *  
  * @param  {string} name the name of the app
@@ -14,7 +14,7 @@ function startApp(name){
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
   console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  console.log('--------------------')
 }
 
 
@@ -27,7 +27,7 @@ function startApp(name){
  * node tasks.js batata
  * ```
  * 
- * The text received would be "batata"
+ * The text received would be 'batata'
  * This function  then directs to other functions
  * 
  * @param  {string} text data typed by the user
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+  help();
+  }
   else{
     unknownCommand(text);
   }
@@ -47,7 +50,7 @@ function onDataReceived(text) {
 
 
 /**
- * prints "unknown command"
+ * prints 'unknown command'
  * This function is supposed to run when all other commands have failed
  *
  * @param  {string} c the text received
@@ -78,5 +81,15 @@ function quit(){
   process.exit();
 }
 
+/**
+ * help
+ */
+function help(){
+
+  console.log('help is to provide any help you need')
+    console.log('hello output hello!')
+      console.log('quit output exit')
+
+}
 // The following line starts the application
-startApp("zahraa alaaeddine")
+startApp('zahraa alaaeddine')
